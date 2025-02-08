0aeabf3a695099a57d402bda115010c6e5850d5b
Luciano Maciel <luchomaciel9@gmail.com>
2025-02-08 12:40:47 -0300

    Added middalware cors

--- 3.1-3.8/index.js
+++ 3.1-3.8/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 const PORT = 3001
 const phone_book = require('./data/phone_book.js')
@@ -8,6 +9,7 @@ morgan.token('body', (req) => {
   return req.method === 'POST' ? JSON.stringify(req.body) : ''
 })
 
+app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body'))
 app.use(express.json())
 

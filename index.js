eec6b19457b7bf2ffa3c934289cf4f141803c426
Luciano Joaquin Maciel <105524431+LucianoMaciel10@users.noreply.github.com>
2025-02-08 19:31:33 -0300

    Add communication between frontend and backend files

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token('body', (req) => {
   return req.method === 'POST' ? JSON.stringify(req.body) : ''
 })
 
+app.use(express.static('dist'))
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body'))
 app.use(express.json())
